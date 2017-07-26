0a7c5edd8ad4f136cc21285a14d91084f171e410
Zombiee K.S <Zombiee K.S>
2017-07-26 18:06:18 -0500

    otro cambio

--- client/assets/js/wallsCardCtrl.js
+++ client/assets/js/wallsCardCtrl.js
@@ -1,6 +1,6 @@
 angular.module('application').controller('wallsCardCtrl',
   ['$scope', '$window', '$state', '$dbApi', 'FoundationApi', function($scope, $window,  $state, $dbApi, FoundationApi){
-  	$scope.screenHeight = (screen.height - 100) + 'px';
+  	$scope.screenHeight = '600px';
   	console.log($scope.screenHeight);
   	$scope.displaySesionN = 'none';
     angular.element(document.querySelector('#selloBounce2s')).addClass('bounceIn2s');
